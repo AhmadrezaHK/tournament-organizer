125a179bb6f73d52863cd1831ed43e738977ef11
ahmadreza <ahmadrezahk23@gmail.com>
2020-06-29 03:12:19 +0430

    delete redundant files

--- src/index.js
+++ src/index.js
@@ -1,17 +1,52 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import ApolloClient from "apollo-boost";
+import { gql } from "apollo-boost";
+import { ApolloProvider, useQuery } from "@apollo/react-hooks";
+
+const client = new ApolloClient({
+    uri: "https://web.devk8s.daory.net/graphql",
+});
+
+const temp = gql`
+    {
+        findTournamentsByString(count: 18, offset: 0, fieldName: "paltform", fieldData: "default") {
+            game {
+                id
+                name
+            }
+            unlisted
+            name
+            description
+            id
+            teamSize
+            maxTeams
+            participantIds
+            tournamentStatus
+            coverImage
+            thumbnail
+            deadline
+            createdAt
+        }
+    }
+`;
+
+function App() {
+    const { loading, error, data } = useQuery(temp);
+
+    console.log(data, loading, error);
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error :(</p>;
+    return <div className="App">sadasdada</div>;
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <ApolloProvider client={client}>
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    </ApolloProvider>,
+    document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
