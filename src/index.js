e277aef507699b9e2d50a25e07b4ceea0bdf2601
ahmadreza <ahmadrezahk23@gmail.com>
2020-06-30 20:44:21 +0430

    extract queries from index.js

--- src/index.js
+++ src/index.js
@@ -2,44 +2,30 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import ApolloClient from "apollo-boost";
-import { gql } from "apollo-boost";
 import { ApolloProvider, useQuery } from "@apollo/react-hooks";
+import TournamentList from "components/TournamentList";
+import NavBar from "components/NavBar"
+import { loader } from 'graphql.macro';
 
 const client = new ApolloClient({
     uri: "https://web.devk8s.daory.net/graphql",
 });
 
-const temp = gql`
-    {
-        findTournamentsByString(count: 18, offset: 0, fieldName: "paltform", fieldData: "default") {
-            game {
-                id
-                name
-            }
-            unlisted
-            name
-            description
-            id
-            teamSize
-            maxTeams
-            participantIds
-            tournamentStatus
-            coverImage
-            thumbnail
-            deadline
-            createdAt
-        }
-    }
-`;
+const tournamentsQuery = loader("./queries/tournamentsQuery.gql");
 
 function App() {
-    const { loading, error, data } = useQuery(temp);
+    const { loading, error, data: { findTournamentsByString: tournaments } = {} } = useQuery(tournamentsQuery);
 
-    console.log(data, loading, error);
 
     if (loading) return <p>Loading...</p>;
     if (error) return <p>Error :(</p>;
-    return <div className="App">sadasdada</div>;
+
+    return (
+        <>
+            <NavBar />
+            <TournamentList tournaments={tournaments} />
+        </>
+    );
 }
 
 ReactDOM.render(
