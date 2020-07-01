9e47e3af2a4a84b3601085731541e9873b2eb6ff
ahmadreza <ahmadrezahk23@gmail.com>
2020-07-01 08:51:10 +0430

    add loading component

---
+++ src/components/Loading/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './index.scss'
+
+export default function Loading (){
+
+    return (
+        <div className="donut-container">
+            <div class="donut"></div>
+        </div>
+    );
+}
