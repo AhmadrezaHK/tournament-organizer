bd3a15833ebf21f53a29f2c12560c893244edec2
ahmadreza <ahmadrezahk23@gmail.com>
2020-07-01 08:49:07 +0430

    add useMq and useMqList

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,13 +1,21 @@
 import { useTheme } from "@material-ui/core/styles";
 import useMediaQuery from "@material-ui/core/useMediaQuery";
 
-export function useMqProperty({ xs, sm, md, lg, xl } = { xs: "", sm: "", md: "", lg: "", xl: "" }) {
-    const mq = {
-        xs: useMediaQuery(useTheme().breakpoints.only("xs")),
-        sm: useMediaQuery(useTheme().breakpoints.only("sm")),
-        md: useMediaQuery(useTheme().breakpoints.only("md")),
-        lg: useMediaQuery(useTheme().breakpoints.only("lg")),
-        xl: useMediaQuery(useTheme().breakpoints.only("xl")),
+export function useMq(type) { //type: 'only', 'up', 'down',
+    return {
+        xs: useMediaQuery(useTheme().breakpoints[type]("xs")),
+        sm: useMediaQuery(useTheme().breakpoints[type]("sm")),
+        md: useMediaQuery(useTheme().breakpoints[type]("md")),
+        lg: useMediaQuery(useTheme().breakpoints[type]("lg")),
+        xl: useMediaQuery(useTheme().breakpoints[type]("xl")),
     };
+}
+
+export function useMqProperty({ xs, sm, md, lg, xl } = { xs: "", sm: "", md: "", lg: "", xl: "" }) {
+    const mq = useMq('only');
     return [mq.xs && xs, mq.sm && sm, mq.md && md, mq.lg && lg, mq.xl && xl].find(Boolean);
+}
+
+export function useMqList() {
+    return useMqProperty({xs: "xs", sm: "sm", md: "md", lg: "lg", xl: "xl"});
 }
