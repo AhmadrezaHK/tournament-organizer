1b9efb0226b345b3b1a551d390382501c20971db
ahmadreza <ahmadrezahk23@gmail.com>
2020-07-01 08:50:25 +0430

    add style to index.scss

--- src/components/TournamentCard/index.js
+++ src/components/TournamentCard/index.js
@@ -5,37 +5,26 @@ import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Box from "@material-ui/core/Box";
 import Typography from "@material-ui/core/Typography";
-import { makeStyles } from "@material-ui/core/styles";
 import dayjs from "dayjs";
-
-const useStyles = makeStyles(theme=>({
-    card: {
-        [theme.breakpoints.only("xs")]: {
-            maxWidth: '260px',
-            marginLeft: 'auto',
-            marginRight: 'auto',
-        },
-    },
-}));
+import './index.scss'
 
 export default function TournamentCard({ tournament }) {
-    const $class = useStyles()
     return (
-        <Card className={$class.card}>
+        <Card className="card">
             <CardActionArea>
-                <CardMedia image={tournament.thumbnail} title="Contemplative Reptile" style={{ height: "12.5rem" }} />
+                <CardMedia image={tournament.thumbnail} title="Contemplative Reptile" className="card-image" />
                 <CardContent>
                     <Typography gutterBottom variant="subtitle2" component="h6">
                         {tournament.game.name}
                     </Typography>
                     <Box mb="1rem">
-                        <Typography noWrap variant="subtitle1" component="h6" style={{ fontWeight: "bold" }}>
+                        <Typography noWrap variant="subtitle1" component="h6" className="tournament-name">
                             {tournament.name}
                         </Typography>
                     </Box>
                     <Box mb="1rem">
                         <Typography gutterBottom variant="body2" component="p">
-                            Date: {dayjs(tournament.deadline).format("MMMM D, YYYY HH:mm A")}
+                            Date: {dayjs(tournament.deadline).format("MMM D, YYYY HH:mm A")}
                         </Typography>
                         <Typography gutterBottom variant="body2" component="p">
                             Team Size: {tournament.teamSize}
